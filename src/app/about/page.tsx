f4ca812fec87f1e677bee3c950ab3056371576b8
Anubhav Singh <anubhavsingbkj@gmail.com>
2024-08-12 14:47:11 +0530

    Resolve

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,14 +3,7 @@ import AboutAvatarSVG from "@/app/about/AboutAvatarSVG";
 import { IoIosStar } from "react-icons/io";
 
 const About = () => {
-  const ACHIEVEMENTS = [
-    // "Achieved Highest Rating of 1766 (Expert) on Codeforces",
-    // "Global Rank 156 in Codeforces Round 875 (Div. 2)",
-    // "Global Rank 527 in Codeforces Round 865 (Div. 2)",
-    // "Achieved Highest Rating of 2029 (5 Star) on CodeChef",
-    // "Global Rank 617 (India Rank 411) in Google Kickstart Round G 2022",
-    // "Guardian (Max Rating 2315) in leetcode",
-  ];
+ ;
 
   return (
     <div className="w-[1200px] max-w-[90%] mx-auto md:mt-10">
@@ -23,16 +16,6 @@ const About = () => {
           <div className="mt-8 text-sm md:text-base">
             {/* <p>Some of my competitive programming achievements are:</p> */}
           </div>
-          <ul>
-            {ACHIEVEMENTS.map((achievement, key) => (
-              <li key={key} className="flex gap-3 md:item-center mt-4">
-                <div>
-                  <IoIosStar size="1.3rem" />
-                </div>
-                <p className="text-sm md:text-base">{achievement}</p>
-              </li>
-            ))}
-          </ul>
         </div>
         <div className="md:w-[40%]">
           <AboutAvatarSVG className="w-[100%]" />
